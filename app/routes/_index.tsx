04a97a93a846e9c841a4de25b567cf57f519777c
Soumen Chakraborty <soumen@adultcreative.com>
2025-12-18 16:31:03 +0530

    logo and design change

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,7 +5,7 @@ import { Chat } from '~/components/chat/Chat.client';
 import { Header } from '~/components/header/Header';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'Bolt' }, { name: 'description', content: 'Talk with Bolt, an AI assistant from StackBlitz' }];
+  return [{ title: 'Best Builder' }, { name: 'description', content: 'Talk with Best Builder, an AI assistant from Best Network' }];
 };
 
 export const loader = () => json({});
