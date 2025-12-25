47fa48be8f90bb33578705735c378b3ce080ae9a
Soumen Chakraborty <soumen@adultcreative.com>
2025-12-26 01:44:21 +0530

    update web container

--- app/lib/webcontainer/auth.client.ts
+++ app/lib/webcontainer/auth.client.ts
@@ -3,4 +3,13 @@
  * to avoid importing `@webcontainer/api` in the server bundle.
  */
 
+import { auth } from '@webcontainer/api';
+
+if (!import.meta.env.SSR) {
+  auth.init({
+    clientId: import.meta.env.VITE_WEBCONTAINER_CLIENT_ID || 'Your_Client_ID',
+    scope: '',
+  });
+}
+
 export { auth, type AuthAPI } from '@webcontainer/api';
